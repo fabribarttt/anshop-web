69b1f51e453c7ac17116b0075cc3d0419dccef5b
Fabricio Bartomeu <86902293+fabribarttt@users.noreply.github.com>
2026-05-06 15:07:16 -0300

    migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    modelos_disponibles = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class ProductoImagen(models.Model):
+    producto = models.ForeignKey(
+        Producto, on_delete=models.CASCADE, related_name="imagenes"
+    )
+    imagen_url = models.URLField()
+    principal = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Imagen de {self.producto.nombre}"
